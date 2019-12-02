f058cdf43589f6acb874a3f710b8041f9155a512
nthnjustice <nthnjustice@gmail.com>
2019-12-01 19:39:26 -0500

    restructured project initialization with class implementation, added padding and polygon fill

--- Code/main.py
+++ Code/main.py
@@ -1,9 +1,15 @@
-import init_directories
-import fetch_data
-import process_data
-import split_data
+import init
 
-init_directories.run()
-fetch_data.run()
-process_data.run()
-split_data.run()
+# SHAPEFILE = 'data/shapefiles/MTPublicLands_SHP/MTPublicLands_SHP.shp'
+# TARGET = 'OWNER'
+SHAPEFILE = 'data/shapefiles/PADUS2_0_Shapefiles/PADUS2_0Fee.shp'
+TARGET = 'Own_Type'
+
+DIMENSION = 240
+PADDING = 10
+MIN_COUNT = 500
+TEST_SIZE = 0.2
+VALID_SIZE = 0.2
+
+dataset = init.Dataset(SHAPEFILE, TARGET, DIMENSION, PADDING, MIN_COUNT, TEST_SIZE, VALID_SIZE)
+dataset.run()
