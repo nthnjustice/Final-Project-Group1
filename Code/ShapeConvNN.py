3c122b659963d840eb24f382302a59f2fb34f465
dvitale199 <d.vitale199@gmail.com>
2019-12-03 21:03:02 -0500

    changes to architecture in ShapeConvNN.py

--- Code/ShapeConvNN.py
+++ Code/ShapeConvNN.py
@@ -8,6 +8,9 @@ from keras.callbacks import ModelCheckpoint
 from keras.utils import model_to_dot
 import matplotlib.pyplot as plt
 from keras import optimizers
+from imblearn.keras import balanced_batch_generator
+from sklearn.utils import class_weight
+import numpy as np
 
 
 path_dir_train = '/home/ubuntu/Deep-Learning/Final-Project-Group1/Code/data/train'
@@ -16,14 +19,14 @@ path_dir_test = '/home/ubuntu/Deep-Learning/Final-Project-Group1/Code/data/test'
 
 img_width = 250
 img_height = 250
-epochs = 60
+epochs = 300
 
 generator = ImageDataGenerator(rescale=1./255)
 
 train_generator = generator.flow_from_directory(
     path_dir_train,
     target_size=(img_width, img_height),
-    batch_size=100,
+    batch_size=150,
     color_mode="grayscale",
     interpolation="lanczos",
     class_mode='categorical')
@@ -32,45 +35,40 @@ train_generator = generator.flow_from_directory(
 validation_generator = generator.flow_from_directory(
     path_dir_validate,
     target_size=(img_width, img_height),
-    batch_size=100,
+    batch_size=150,
     color_mode="grayscale",
     interpolation="lanczos",
     class_mode='categorical')
 
+class_weights = class_weight.compute_class_weight(
+           'balanced',
+            np.unique(train_generator.classes),
+            train_generator.classes)
+
 # #define model
 model = Sequential()
-model.add(Convolution2D(32, 5, 5, input_shape=(img_width, img_height,1)))
+model.add(Convolution2D(32, 20, 20, input_shape=(img_width, img_height,1)))
 model.add(Activation('relu'))
-model.add(Dropout(0.2))
+# model.add(Dropout(0.2))
+model.add(MaxPooling2D(pool_size=(10, 10)))
+
+model.add(Convolution2D(32, 3, 3))
+model.add(Activation('relu'))
+# model.add(Dropout(0.2))
 model.add(MaxPooling2D(pool_size=(3, 3)))
 
-model.add(Convolution2D(32, 2, 2))
-model.add(Activation('relu'))
-model.add(Dropout(0.2))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-# model.add(Convolution2D(32, 3, 3))
-# model.add(Activation('relu'))
-# model.add(Dropout(0.2))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
-
-# model.add(Convolution2D(32, 3, 3))
-# model.add(Activation('relu'))
-# model.add(Dropout(0.2))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
-
 model.add(Flatten())
-# model.add(Dense(100))
-model.add(Dense(50))
+# model.add(Dense(16))
+model.add(Dense(10))
 model.add(Activation('relu'))
-model.add(Dropout(0.2))
+model.add(Dropout(0.50))
 
-model.add(Dense(5))
+model.add(Dense(4))
 
 model.add(Activation('softmax'))
-
-
-AdamOP=optimizers.adam(lr=0.001)
+Adagrad = optimizers.Adagrad()
+RMSprop = optimizers.RMSprop()
+AdamOP=optimizers.adam(lr=0.0001)
 model.summary()
 model.compile(optimizer=AdamOP,
               loss='categorical_crossentropy',
@@ -78,9 +76,10 @@ model.compile(optimizer=AdamOP,
 
 history = model.fit_generator(
     train_generator,
+    # class_weight=class_weights,
     nb_epoch=epochs,
     validation_data=validation_generator,
-    callbacks=[ModelCheckpoint("/home/ubuntu/Deep-Learning/Final-Project-Group1/models/taxa_area_prelim.hdf5",
+    callbacks=[ModelCheckpoint("/home/ubuntu/Deep-Learning/Final-Project-Group1/models/taxa_area_prelim_300epoch.hdf5",
                                monitor="val_loss", save_best_only=True)]
 )
 
@@ -91,7 +90,7 @@ plt.title('Model accuracy')
 plt.ylabel('Accuracy')
 plt.xlabel('Epoch')
 plt.legend(['Train', 'Test'], loc='upper left')
-plt.savefig('acc_taxa_area_prelim.png')
+plt.savefig('acc_taxa_area_prelim_300epoch.png')
 plt.show()
 
 # Plot training & validation loss values
@@ -102,5 +101,5 @@ plt.ylabel('Loss')
 plt.xlabel('Epoch')
 plt.legend(['Train', 'Test'], loc='upper left')
 
-plt.savefig('val_taxa_area_prelim.png')
+plt.savefig('val_taxa_area_prelim_300epoch.png')
 plt.show()
