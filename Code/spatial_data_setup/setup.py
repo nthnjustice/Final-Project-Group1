6109547b864230eebedf4cb8bcba5c4cdbf1f5cb
nthnjustice <nthnjustice@gmail.com>
2019-12-07 15:02:13 -0500

    added module init files

--- Code/spatial_data_setup/setup.py
+++ Code/spatial_data_setup/setup.py
@@ -1,8 +1,8 @@
 from utils.get_datainfo import get_datainfo
 from utils.init_dir import init_dir
-from setup.fetch_data import fetch_data
-from setup.sp_2_png import sp_2_png
-from setup.split_data import split_data
+from spatial_data_setup.fetch_data import fetch_data
+from spatial_data_setup.sp_2_png import sp_2_png
+from spatial_data_setup.split_data import split_data
 
 data_source = 'buildings'
 target = None
